79a4f3c5d580305ee55743f40d7e930954c653b7
Mehul Lakhanpal <318097@gmail.com>
2020-05-03 12:01:08 +0530

    Add: Imported UI components from brainbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="app">
-      <Switch>{/* <Route exact path="/home" component={Home} /> */}</Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, Card, Tag, Input } from "../UIComponents";
+import "./Home.scss";
+
+const UIComponent = ({ type }) => {
+  switch (type) {
+    case "BUTTON":
+      return <Button>Test</Button>;
+    case "CARD":
+      return <Card>Test file</Card>;
+    case "TAG":
+      return <Tag>Test file</Tag>;
+    case "INPUT":
+      return <Input />;
+    default:
+      return null;
+  }
+};
+
+const uiList = ["BUTTON", "CARD", "TAG", "INPUT"];
+
+const Home = () => {
+  return (
+    <div className="ui-container">
+      {uiList.map((type) => (
+        <div key={type} className="item">
+          <UIComponent type={type} />
+          <span className="name">{type}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
