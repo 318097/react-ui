0b2e29bac9e8dd76036cb4fed6c1ffa30ae8da43
Mehul Lakhanpal <318097@gmail.com>
2021-06-19 20:09:25 +0530

    Update: Check-in dist to git

---
+++ dist/styles/colors.js
@@ -0,0 +1,42 @@
+export default {
+  primary: "#2c3d63",
+  red: "#B11E31",
+  watermelon: "#f75961",
+  blue: "#2dadba",
+  orchid: "#6464dc",
+  yellow: "#ffb400",
+  orange: "#f57b39",
+  green: "#3CAD99",
+  coffee: "#4F3B38",
+
+  bar: "#202632",
+  black: "#282828",
+  black2: "#353535",
+  charcoal: "#2b2b2b",
+
+  lead: "#393939",
+  slate: "#484848",
+  iron: "#686868",
+  steel: "#797979",
+  chrome: "#bcbcbc",
+
+  grey: "#ccc",
+  strokeThree: "#d9d9d9",
+  strokeTwo: "#d3d3d3",
+
+  strokeOne: "#ebebeb",
+  shade1: "#f3f3f3",
+  bg: "whitesmoke",
+  shade2: "#f9f9f9",
+  featherDark: "#fafafa",
+  feather: "#fbfbfb",
+
+  white: "#ffffff",
+
+  nbOrange: "#FF7A00",
+  nbPink: "#FF5151",
+  foBlue: "#52ADA2",
+  cdBlue: "#1E2F49",
+  cdGreen: "#00A962",
+  purple: "#908DB9",
+};
---
+++ dist/styles/constants.js
@@ -0,0 +1,9 @@
+import colors from "./colors";
+
+export default {
+  BORDER_RADIUS: "2px",
+  FONT_SIZE: "1.2rem",
+  PRIMARY: colors.bar,
+  SECONDARY: colors.strokeTwo,
+  BG: colors.strokeOne,
+};
