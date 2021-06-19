256a87d099b06a688bdb33ccc105a384b856d808
Mehul Lakhanpal <318097@gmail.com>
2021-06-19 23:41:58 +0530

    Test

--- webpack.lib.js
+++ webpack.lib.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    libraryTarget: "commonjs2",
+    libraryTarget: "umd",
     library: "reactUI",
   },
   externals: {
