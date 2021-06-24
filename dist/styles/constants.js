cacb54ab836c1453a3808b0ec3b8b450cd0278f6
Mehul Lakhanpal <318097@gmail.com>
2021-06-24 19:52:15 +0530

    Add: /dist to .gitignore

--- dist/styles/constants.js
+++
@@ -1,9 +0,0 @@
-import colors from "./colors";
-
-export default {
-  BORDER_RADIUS: "2px",
-  FONT_SIZE: "1.2rem",
-  PRIMARY: colors.bar,
-  SECONDARY: colors.strokeTwo,
-  BG: colors.strokeOne,
-};
