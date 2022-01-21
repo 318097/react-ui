dcfa56575b67a79cafac548e74ac95a4c82ce638
Mehul Lakhanpal <318097@gmail.com>
2022-01-21 20:53:39 +0530

    fix: fake commit

--- webpack.lib.js
+++ webpack.lib.js
@@ -2,6 +2,7 @@ const path = require("path");
 // const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const commonConfig = require("./webpack.common");
+// fake commit
 
 module.exports = () => {
   return {
@@ -18,7 +19,7 @@ module.exports = () => {
       react: "react",
       "react-dom": "react-dom",
     },
-  plugins: [new CopyPlugin([{ from: "./src/magicdust", to: "./styles/" }])],
+    plugins: [new CopyPlugin([{ from: "./src/magicdust", to: "./styles/" }])],
     // optimization: {
     //   minimize: true,
     //   minimizer: [
